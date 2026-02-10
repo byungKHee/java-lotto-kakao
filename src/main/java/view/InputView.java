b845ce3e278e1edca9a678875eeae6cd5f5b1ccc
byungKHee <byungheekang@g.skku.edu>
2026-02-10 14:41:36 +0900

    feat: InputView 구현

---
+++ src/main/java/view/InputView.java
@@ -0,0 +1,63 @@
+package view;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class InputView {
+
+    public InputView() {}
+
+    public int enterPurchaseAmount() {
+        System.out.println("구입금액을 입력해 주세요.");
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine();
+
+        validateNumber(input);
+
+        int amount = Integer.parseInt(input);
+        if (amount <= 0) {
+            throw new IllegalArgumentException("구입 금액은 양수여야 합니다.");
+        }
+        return amount;
+    }
+
+    public List<Integer> enterWinningNumbers() {
+        System.out.println("지난 주 당첨 번호를 입력해 주세요.");
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine();
+
+        String[] parts = input.split(",");
+        if (parts.length != 6) {
+            throw new IllegalArgumentException("당첨 번호는 6개여야 합니다.");
+        }
+
+        for (String part : parts) {
+            validateNumber(part);
+        }
+
+        List<Integer> winningNumbers = new ArrayList<>();
+        for (String part : parts){
+            winningNumbers.add(Integer.parseInt(part.trim()));
+        }
+        return winningNumbers;
+    }
+
+    public int enterBonusNumber() {
+        System.out.println("보너스 볼을 입력해 주세요.");
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine();
+
+        validateNumber(input);
+
+        return Integer.parseInt(input);
+    }
+
+
+    private void validateNumber(String str) {
+        if(!str.trim().matches("\\d+")){
+            throw new IllegalArgumentException("입력 값이 숫자가 아닙니다.");
+        }
+    }
+
+}
