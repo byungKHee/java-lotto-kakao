6911c2edb7db7b1943ccdef64a24dc4550406a60
byungKHee <byungheekang@g.skku.edu>
2026-02-12 15:24:43 +0900

    feat: 수동 구매 컨트롤러 추가

--- src/main/java/controller/LottoController.java
+++ src/main/java/controller/LottoController.java
@@ -20,10 +20,26 @@ public class LottoController {
         this.outputView = outputView;
     }
 
-    public void run() {
+    public void run_step1() {
         int purchaseAmount = inputView.enterPurchaseAmount();
         LottoService lottoService = new LottoService(new LottoFactory());
-        LottoGroup lottoGroup = lottoService.issueLottoGroup(purchaseAmount);
+        LottoGroup lottoGroup = lottoService.issueAutoLottoGroup(purchaseAmount);
+        outputView.printLottoCount(lottoGroup);
+
+        List<Integer> winningNumbers = inputView.enterWinningNumbers();
+        int bonus = inputView.enterBonusNumber();
+        WinningLotto winningLotto = new WinningLotto(winningNumbers, bonus);
+        LottoResult lottoResult = lottoService.calculateResult(lottoGroup, winningLotto);
+        outputView.printStatistics(lottoResult);
+    }
+
+    public void run_step2() {
+        int purchaseAmount = inputView.enterPurchaseAmount();
+        int manualCount = inputView.enterManualPurchaseCount();
+        List<List<Integer>> manualNumbers = inputView.enterManualLottos(manualCount);
+
+        LottoService lottoService = new LottoService(new LottoFactory());
+        LottoGroup lottoGroup = lottoService.issueMixedLottoGroup(purchaseAmount, manualNumbers);
         outputView.printLottoCount(lottoGroup);
 
         List<Integer> winningNumbers = inputView.enterWinningNumbers();
